ca1ae49757f3a8b5f4ec772846828bb525fd22a7
nathan sodja <sodjanathan@gmail.com>
2023-01-18 02:51:30 +0000

    feat: pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -38,10 +38,22 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const allJobs = await result;
 
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
   // allJobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
-  res.status(StatusCodes.OK).json({ allJobs, count: allJobs.length });
+  res
+    .status(StatusCodes.OK)
+    .json({ count: allJobs.length, numOfPages, allJobs });
 };
 
 // get a new job with id and createdBy
