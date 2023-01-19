18c64506d2c1df5bf70264d710863fd0a0bc3a62
nathan sodja <sodjanathan@gmail.com>
2023-01-19 14:52:53 +0000

    feat: setup aggregation pipeline

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,8 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
+const mongoose = require("mongoose");
+const moment = require("moment");
 
 // get all jobs
 const getAllJobs = async (req, res) => {
@@ -126,6 +128,12 @@ const deleteJob = async (req, res) => {
 // showStats
 
 const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+
+  console.log(stats);
   res
     .status(StatusCodes.OK)
     .json({ defaultStats: {}, monthlyApplications: [] });
