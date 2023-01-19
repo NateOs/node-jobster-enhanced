a0e0f27adbf5b2fa4738008a1829c305f27df359
nathan sodja <sodjanathan@gmail.com>
2023-01-19 15:09:52 +0000

    feat: stats aggregation complete

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -133,10 +133,22 @@ const showStats = async (req, res) => {
     { $group: { _id: "$status", count: { $sum: 1 } } },
   ]);
 
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
   console.log(stats);
-  res
-    .status(StatusCodes.OK)
-    .json({ defaultStats: {}, monthlyApplications: [] });
+
+  const defaultStats = {
+    // failsafe for when there's no stats available
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications: [] });
 };
 
 module.exports = {
